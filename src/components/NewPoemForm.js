53b3ddc6a24b296d8373b5a958349a12e9b09bf2
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-07-24 13:35:29 +0300

    Completed a post

--- src/components/NewPoemForm.js
+++ src/components/NewPoemForm.js
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
+import React, {useState } from "react";
 
 function NewPoemForm() {
 const [poemFormData,setPoemData]= useState({title:"",content: "",author: ""});
 
 function handleInputChange(event){
   setPoemData({...poemFormData,[event.target.name]:event.target.value})
-  console.log(poemFormData)
+}
+
+function handleFormSubmit(event){
+  event.preventDefault();
+  fetch('http://localhost:8004/poems',{
+    method:'POST',
+    headers:{
+      'Content-Type':'Application/json',
+      'Accept':'Application/json'
+    },
+    body:JSON.stringify(poemFormData)
+  })
+  .then(response=>response.json())
+  .then(data=>{
+    setPoemData({title:"",content: "",author: ""})
+  })
+  .catch(error=>console.log(error))
 }
 
   return (
-    <form className="new-poem-form">
+    <form className="new-poem-form" onSubmit={handleFormSubmit}>
       <input placeholder="Title" name="title" value={poemFormData.title} onChange={handleInputChange}/>
       <input placeholder="Author" name="author" value={poemFormData.author} onChange={handleInputChange}/>
       <textarea placeholder="Write your masterpiece here..." rows={10} name="content" value={poemFormData.content} onChange={handleInputChange}/>
